b1d3d8ce8d163c31eb7eaace5ce1a97dfd26583d
George Saunders <kvnrs711@gmail.com>
2025-08-29 12:18:52 -0700

    file finder

--- main.lua
+++ main.lua
@@ -1,1 +1,11 @@
-print("Hello World!")
+local filename = io.read()
+
+local file = io.open(filename)
+if file then
+	local words = file:read("a")
+	print(words)
+else
+	print("Could not find file:")
+	print(filename)
+end
+
